284e54dde4a456e4c7e4a460de6fb1f5febe92e7
MrWalshyType2 <morgan.walsh@academytrainee.com>
2020-07-03 14:13:17 +0100

    Added anon inner class example to show bad practice

--- src/com/qa/hello_world/App.java
+++ src/com/qa/hello_world/App.java
@@ -4,7 +4,7 @@ public class App {
 	interface SayHello {
 		void sayMessage();
 	}
-	
+
 	public static void main(String args[]) {
 		Message messenger = new Message();
 		Math calculator = new Math();
@@ -12,32 +12,43 @@ public class App {
 		messenger.operation("Fred says hi!", messenger.sayMessage);
 		messenger.operation("Bob says hi", messenger.sayMessage);
 		messenger.operation("Bob", messenger.greet);
-		
+
 		System.out.println(calculator.operation(10, 10, calculator.addition));
-		
+
 		///////////////////////////////////////////////////
-		// Anonymous inner interface                     //
+		// Anonymous inner interface //
 		///////////////////////////////////////////////////
-		SayHello h;
 		SayHello i = new SayHello() { // DO NOT DO THIS
 			// DO NOT DO THIS
-			@Override// DO NOT DO THIS
+			@Override // DO NOT DO THIS
 			public void sayMessage() {// DO NOT DO THIS
 				// TODO Auto-generated method stub// DO NOT DO THIS
 				// DO NOT DO THIS
 			}// DO NOT DO THIS
 		};
-		h = () -> { System.out.println("Hello"); };
-		
+
+		SayHello h;
+		h = () -> {
+			System.out.println("Hello");
+		};
+
 		h.sayMessage();
-		
-		Consumer.ConsumerMath addition = (num1, num2) -> { return num1 + num2; };
-		Consumer.ConsumerMath subtraction = (num1, num2) -> { return num1 - num2; };
-		Consumer.ConsumerMath multiplication = (num1, num2) -> { return num1 * num2; };
-		Consumer.ConsumerMath division = (num1, num2) -> { return num1 / num2; };
+
+		Consumer.ConsumerMath addition = (num1, num2) -> {
+			return num1 + num2;
+		};
+		Consumer.ConsumerMath subtraction = (num1, num2) -> {
+			return num1 - num2;
+		};
+		Consumer.ConsumerMath multiplication = (num1, num2) -> {
+			return num1 * num2;
+		};
+		Consumer.ConsumerMath division = (num1, num2) -> {
+			return num1 / num2;
+		};
 		System.out.println(addition.doMath(20, 20));
 		System.out.println(subtraction.doMath(20, 10));
-		
+
 		Consumer.ConsumerMessage publicServiceAnnouncement1 = (message) -> {
 			System.out.println(message);
 		};
--- src/com/qa/hello_world/Consumer.java
+++ src/com/qa/hello_world/Consumer.java
@@ -1,11 +1,11 @@
 package com.qa.hello_world;
 
 public interface Consumer {
-	
+
 	interface ConsumerMessage {
 		void sayMessage(String message);
 	}
-	
+
 	interface ConsumerMath {
 		int doMath(int num1, int num2);
 	}
